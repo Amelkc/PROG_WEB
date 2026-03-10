97e74683ab983a8e8fbec779d624b4e1fb78f66a
Amel Kaci <amel.kaci@etu.u-paris.fr>
2026-03-10 19:35:36 +0100

    feat: add models 'Participant' 'Event' 'Registration'

--- event_hub/eventHub/models.py
+++ event_hub/eventHub/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+class Participant(models.models):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Event(models.models):
+    
+    
+    STATUS_CHOICES = [
+        ("cancelled", "Cancelled"), 
+        ("postponed", "Postponed"), #date modifiée, potentielleme,nt devenue nulle 
+        ("open", "Open"), #distinction avec ongoing ?
+        ("closed", "Closed") 
+    ]
+    title = models.CharField(max_length=80)
+    description = models.TextField(blank=True)
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
+    participants = models.ManyToManyField(
+        Participant,
+        through="Registration",
+        related_name="events",
+    )
+    def __str__(self):
+        return self.title
+    
+
+
+class Registration(models.models):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    participant = models.ForeignKey(Participant, on_delete=models.CASCADE)
+    class Meta:
+        unique_together = [["event", "participant"]]
+   
