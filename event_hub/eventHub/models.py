803088fe8aa96bb35c9e6f455ecae3193bef92d0
Amel Kaci <amel.kaci@etu.u-paris.fr>
2026-03-13 16:03:57 +0100

    feat: participant extends abstractuser and fields added to event

--- event_hub/eventHub/models.py
+++ event_hub/eventHub/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-class Participant(models.models):
+class Participant(AbstractUser):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
@@ -18,12 +22,18 @@ class Event(models.models):
     ]
     title = models.CharField(max_length=80)
     description = models.TextField(blank=True)
+    location =  models.CharField(max_length=80)
     start_datetime = models.DateTimeField()
     end_datetime = models.DateTimeField()
     participants = models.ManyToManyField(
         Participant,
         through="Registration",
         related_name="events",
+    )
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default="open",
     )
     def __str__(self):
         return self.title
