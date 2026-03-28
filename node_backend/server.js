3ee0dad50a5ed63af474e510f4db817887da857c
Napacher <nic.xu@etu.u-paris.fr>
2026-03-28 03:05:38 +0100

    chore: Initial node setup with express and sqlite + gitignore changes

---
+++ node_backend/server.js
@@ -0,0 +1,18 @@
+//https://www.youtube.com/watch?v=SccSCuHhOw0
+
+const express = require('express');
+//const cors = require('cors');
+
+const app = express();
+
+// app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('EventHub API is running');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
