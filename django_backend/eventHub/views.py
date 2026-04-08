74f797aa1f6feb653a0ebf79522fffc138d0f361
Amel Kaci <amel.kaci@etu.u-paris.fr>
2026-04-08 16:59:05 +0200

    fix: change to viewset

--- django_backend/eventHub/views.py
+++ django_backend/eventHub/views.py
@@ -1,127 +1,108 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from .models import *
 from .serializers import *
-from rest_framework.decorators import api_view
-from rest_framework import status
 from .filters import EventFilter
-from django.shortcuts import get_object_or_404
-
-# Create your views here.
-# CRUD = Create Read Update Delete
-#401 -> unauthorized request
 
 
-#Event
-@api_view(['GET', 'POST'])
-def event_list_create(request):
-    if request.method == 'GET':
-        events = Event.objects.all()
-        filterset = EventFilter(request.query_params, queryset=events)
+class EventViewSet(viewsets.ViewSet):
+
+    def list(self, request):
+        queryset = Event.objects.all()
+        filterset = EventFilter(request.query_params, queryset=queryset)
         if filterset.is_valid():
-            events = filterset.qs
-        serializer = EventSerializer(events, many=True)
+            queryset = filterset.qs
+        serializer = EventSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
- 
-    if request.method == 'POST':
+
+    def create(self, request):
         serializer = EventSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
- 
- 
-@api_view(['GET', 'PUT', 'DELETE'])
-def event_detail(request, pk):
-    event = get_object_or_404(Event, pk=pk)
- 
-    if request.method == 'GET':
+
+    def retrieve(self, request, pk=None):
+        event = get_object_or_404(Event, pk=pk)
         serializer = EventSerializer(event)
         return Response(serializer.data, status=status.HTTP_200_OK)
- 
-    if request.method  == 'PUT':
+
+    def update(self, request, pk=None):
+        event = get_object_or_404(Event, pk=pk)
         serializer = EventSerializer(instance=event, data=request.data, partial=False)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
- 
-    if request.method == 'DELETE':
+
+    def destroy(self, request, pk=None):
+        event = get_object_or_404(Event, pk=pk)
         event.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
- 
 
 
+class ParticipantViewSet(viewsets.ViewSet):
 
-#Participant
-@api_view(['GET', 'POST'])
-def participant_list_create(request):
-    if request.method == 'GET':
-        participants = Participant.objects.all()
-        serializer = ParticipantSerializer(participants, many=True)
+    def list(self, request):
+        queryset = Participant.objects.all()
+        serializer = ParticipantSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
- 
-    if request.method == 'POST':
+
+    def create(self, request):
         serializer = ParticipantSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
- 
- 
-@api_view(['GET', 'PUT', 'DELETE'])
-def participant_detail(request, pk):
-    participant = get_object_or_404(Participant, pk=pk)
- 
-    if request.method == 'GET':
+
+    def retrieve(self, request, pk=None):
+        participant = get_object_or_404(Participant, pk=pk)
         serializer = ParticipantSerializer(participant)
         return Response(serializer.data, status=status.HTTP_200_OK)
- 
-    if request.method == 'PUT':
+
+    def update(self, request, pk=None):
+        participant = get_object_or_404(Participant, pk=pk)
         serializer = ParticipantSerializer(instance=participant, data=request.data, partial=False)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
- 
-    if request.method == 'DELETE':
+
+    def destroy(self, request, pk=None):
+        participant = get_object_or_404(Participant, pk=pk)
         participant.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-#Registration
+class RegistrationViewSet(viewsets.ViewSet):
 
-@api_view(['GET', 'POST'])
-def registration_list_create(request):
-    if request.method == 'GET':
-        registrations = Registration.objects.all()
-        serializer = RegistrationSerializer(registrations, many=True)
+    def list(self, request):
+        queryset = Registration.objects.all()
+        serializer = RegistrationSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
- 
-    if request.method == 'POST':
+
+    def create(self, request):
         serializer = RegistrationSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
- 
- 
-@api_view(['GET', 'PUT', 'DELETE'])
-def registration_detail(request, pk):
-    registration = get_object_or_404(Registration, pk=pk)
- 
-    if request.method == 'GET':
+
+    def retrieve(self, request, pk=None):
+        registration = get_object_or_404(Registration, pk=pk)
         serializer = RegistrationSerializer(registration)
         return Response(serializer.data, status=status.HTTP_200_OK)
- 
-    if request.method == 'PUT':
+
+    def update(self, request, pk=None):
+        registration = get_object_or_404(Registration, pk=pk)
         serializer = RegistrationSerializer(instance=registration, data=request.data, partial=False)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
- 
-    if request.method == 'DELETE':
+
+    def destroy(self, request, pk=None):
+        registration = get_object_or_404(Registration, pk=pk)
         registration.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
- 
