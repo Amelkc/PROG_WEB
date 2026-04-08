877bb3131a67c6709fef2c4a148fcdaf3d3eee7f
Napacher <nic.xu@etu.u-paris.fr>
2026-04-08 19:31:05 +0200

    fix: change to ModelViewSet based on WP7

--- django_backend/eventHub/views.py
+++ django_backend/eventHub/views.py
@@ -1,108 +1,31 @@
-from rest_framework import viewsets, status
-from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
-from .models import *
-from .serializers import *
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, SAFE_METHODS, BasePermission
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Event, Participant, Registration
+from .serializers import EventSerializer, ParticipantSerializer, RegistrationSerializer
 from .filters import EventFilter
 
 
-class EventViewSet(viewsets.ViewSet):
-
-    def list(self, request):
-        queryset = Event.objects.all()
-        filterset = EventFilter(request.query_params, queryset=queryset)
-        if filterset.is_valid():
-            queryset = filterset.qs
-        serializer = EventSerializer(queryset, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def create(self, request):
-        serializer = EventSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def retrieve(self, request, pk=None):
-        event = get_object_or_404(Event, pk=pk)
-        serializer = EventSerializer(event)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def update(self, request, pk=None):
-        event = get_object_or_404(Event, pk=pk)
-        serializer = EventSerializer(instance=event, data=request.data, partial=False)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def destroy(self, request, pk=None):
-        event = get_object_or_404(Event, pk=pk)
-        event.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class ReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS
 
 
-class ParticipantViewSet(viewsets.ViewSet):
-
-    def list(self, request):
-        queryset = Participant.objects.all()
-        serializer = ParticipantSerializer(queryset, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def create(self, request):
-        serializer = ParticipantSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def retrieve(self, request, pk=None):
-        participant = get_object_or_404(Participant, pk=pk)
-        serializer = ParticipantSerializer(participant)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def update(self, request, pk=None):
-        participant = get_object_or_404(Participant, pk=pk)
-        serializer = ParticipantSerializer(instance=participant, data=request.data, partial=False)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def destroy(self, request, pk=None):
-        participant = get_object_or_404(Participant, pk=pk)
-        participant.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class EventViewSet(ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser | ReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = EventFilter  # ← ton filtre est conservé !
 
 
-class RegistrationViewSet(viewsets.ViewSet):
+class ParticipantViewSet(ModelViewSet):
+    queryset = Participant.objects.all()
+    serializer_class = ParticipantSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser | ReadOnly]
 
-    def list(self, request):
-        queryset = Registration.objects.all()
-        serializer = RegistrationSerializer(queryset, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def create(self, request):
-        serializer = RegistrationSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def retrieve(self, request, pk=None):
-        registration = get_object_or_404(Registration, pk=pk)
-        serializer = RegistrationSerializer(registration)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def update(self, request, pk=None):
-        registration = get_object_or_404(Registration, pk=pk)
-        serializer = RegistrationSerializer(instance=registration, data=request.data, partial=False)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def destroy(self, request, pk=None):
-        registration = get_object_or_404(Registration, pk=pk)
-        registration.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class RegistrationViewSet(ModelViewSet):
+    queryset = Registration.objects.all()
+    serializer_class = RegistrationSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser | ReadOnly]
