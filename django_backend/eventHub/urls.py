8e331e84d17d89f7400f3a7743394c0e155caa23
Napacher <nic.xu@etu.u-paris.fr>
2026-04-08 23:13:38 +0200

    fix: add basename to registrations route in urls.py

--- django_backend/eventHub/urls.py
+++ django_backend/eventHub/urls.py
@@ -4,6 +4,6 @@ from .views import *
 router = DefaultRouter()
 router.register(r'events', EventViewSet)
 router.register(r'participants', ParticipantViewSet)
-router.register(r'registrations', RegistrationViewSet)
+router.register(r'registrations', RegistrationViewSet, basename='registration')
 
 urlpatterns = router.urls
