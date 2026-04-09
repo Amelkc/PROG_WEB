2f718b409774285a6699f45d2bda722e6a371518
Napacher <nic.xu@etu.u-paris.fr>
2026-04-09 09:40:34 +0200

    feat: new function IsRegisteredToEvent in permissions.by

--- django_backend/eventHub/permissions.py
+++ django_backend/eventHub/permissions.py
@@ -6,6 +6,7 @@ class ReadOnly(BasePermission):
 
 class IsOwnerPermission(BasePermission):
     def has_permission(self, request, view):
+        # No point to check further if user is not authenticated
         if not request.user.is_authenticated:
             return False
         if view.action == 'create':
@@ -14,3 +15,14 @@ class IsOwnerPermission(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return obj.participant == request.user
+    
+class IsRegisteredToEvent(BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated 
+
+    def has_object_permission(self, request, view, obj) : 
+        # Staff members are allowed to see all events' participants
+        if request.user.is_staff:
+            return True
+        return obj.registration_set.filter(participant=request.user).exists()
+    
