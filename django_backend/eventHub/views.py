7252d58c794b0193e1dac066de914e986f2f59a9
Napacher <nic.xu@etu.u-paris.fr>
2026-04-09 09:42:24 +0200

    fix: use previous feat to limit access of the registration list to registered and admin only

--- django_backend/eventHub/views.py
+++ django_backend/eventHub/views.py
@@ -1,10 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, BasePermission, AllowAny
 from django_filters.rest_framework import DjangoFilterBackend
-from .permissions import ReadOnly, IsOwnerPermission
+from .permissions import ReadOnly, IsOwnerPermission, IsRegisteredToEvent
 from .models import Event, Participant, Registration
 from .serializers import EventSerializer, ParticipantSerializer, RegistrationSerializer
 from .filters import EventFilter
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class EventViewSet(ModelViewSet):
     queryset = Event.objects.all()
@@ -12,6 +14,19 @@ class EventViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated, IsAdminUser | ReadOnly]
     filter_backends = [DjangoFilterBackend]
     filterset_class = EventFilter  
+
+    def get_permissions(self):
+        if self.action == 'participants':
+            return [IsAuthenticated(), IsRegisteredToEvent()]
+        return super().get_permissions()
+
+    @action(detail=True, methods=['get'], url_path='participants')
+    def participants(self, request, pk=None):
+        event = self.get_object()
+        self.check_object_permissions(request, event)  # force le check
+        participants = event.participants.all()
+        serializer = ParticipantSerializer(participants, many=True)
+        return Response(serializer.data)
 
 
 class ParticipantViewSet(ModelViewSet):
