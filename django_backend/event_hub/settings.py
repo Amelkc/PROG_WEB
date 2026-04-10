7248a1f2d6c59e5633b162cac3f4ae6f9944cde8
Amel Kaci <amel.kaci@etu.u-paris.fr>
2026-04-10 05:44:16 +0200

    fix:deploy

--- django_backend/event_hub/settings.py
+++ django_backend/event_hub/settings.py
@@ -57,7 +57,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ROOT_URLCONF = 'event_hub.urls'
 
