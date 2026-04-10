0c2292c4ab5cf7236b82c3797c21c92c1cbddc26
Amel Kaci <amel.kaci@etu.u-paris.fr>
2026-04-10 04:22:52 +0200

    prepare for deploy

--- django_backend/event_hub/settings.py
+++ django_backend/event_hub/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from dotenv import load_dotenv
+load_dotenv()
+import os
+import dj_database_url 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure--ar0d05yy^=!7+m1&z*^d1e1-f)(*pc7%3xhr_cq0^z1d+4tv4'
-
+EXT_URL = os.getenv("EXTERNAL_DB_URL")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -84,7 +88,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+DATABASES['default'] = dj_database_url.parse(EXT_URL)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
